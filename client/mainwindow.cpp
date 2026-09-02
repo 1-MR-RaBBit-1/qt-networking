4a3161ddbba97f206b2cec7046c25da699ab21a4
1-MR-RaBBit-1 <163560416+1-MR-RaBBit-1@users.noreply.github.com>
2026-09-02 10:12:10 +0330

    Add files via upload

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -1,14 +1,31 @@
 #include "mainwindow.h"
+#include <QDataStream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QWidget *centralW = new QWidget(this);
+    setCentralWidget(centralW);
+    QHBoxLayout *HB1 = new QHBoxLayout(centralW);
+    QLineEdit *message = new QLineEdit(centralW);
+    message->setPlaceholderText("pls enter somthing");
+    QPushButton *send = new QPushButton("send",centralW);
+    HB1->addWidget(message);
+    HB1->addWidget(send);
+
     socket = new QTcpSocket(this);
 
     socket->connectToHost(QHostAddress::LocalHost, 5000);
 
     connect(socket, &QTcpSocket::connected, this, [=]() {
         qDebug() << "Connected to server!";
+        // QString text = QInputDialog::getText(this,"Send Data","Enter something:");
+        connect(send,&QPushButton::clicked,this,[=](){
+            QDataStream stream(socket);
+            QString text = message->text();
+            stream << text;
+            message->clear();
+        });
     });
 }
 
--- client/mainwindow.h
+++ client/mainwindow.h
@@ -3,6 +3,12 @@
 
 #include <QMainWindow>
 #include <QTcpSocket>
+#include <QInputDialog>
+#include <QDebug>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QWidget>
 
 class MainWindow : public QMainWindow
 {
